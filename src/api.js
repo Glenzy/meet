ae4b8a7103be45add0223c4a4d0427bd72bcd78d
Glen <glenrcoffey@gmail.com>
2020-08-13 07:48:29 +1000

    4.9B

--- src/api.js
+++ src/api.js
@@ -44,9 +44,13 @@ const extractLocations = (events) => {
 
 const getEvents = async (max_results = 32) => {
   NProgress.start();
-  const events = await localStorage.getItem("lastEvents");
-  NProgress.done();
-  return { events: JSON.parse(events).events, locations: extractLocations(JSON.parse(events).events) };
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return  { events: JSON.parse(events), locations: extractLocations(events) };
+  }
+
   if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return { events: mockEvents, locations: extractLocations(mockEvents) };
