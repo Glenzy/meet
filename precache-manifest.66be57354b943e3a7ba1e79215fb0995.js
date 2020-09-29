0b70a063eeaa9980ff31e2437b14490c83eed3ff
Glen <glenrcoffey@gmail.com>
2020-09-29 21:09:27 +1000

    Updates

---
+++ precache-manifest.66be57354b943e3a7ba1e79215fb0995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fce36b402eef71e9ebd1b76ba055186",
+    "url": "/index.html"
+  },
+  {
+    "revision": "991810dadee7b4d74c7d",
+    "url": "/static/css/main.6f936b22.chunk.css"
+  },
+  {
+    "revision": "981f80cef67ea8ed1789",
+    "url": "/static/js/2.69b44289.chunk.js"
+  },
+  {
+    "revision": "baf8fb70375478a7e271cd4e6846676c",
+    "url": "/static/js/2.69b44289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991810dadee7b4d74c7d",
+    "url": "/static/js/main.843d3bfb.chunk.js"
+  },
+  {
+    "revision": "15ec8bacbf308d431d6b",
+    "url": "/static/js/runtime-main.0bc04a24.js"
+  }
+]);
